c0468dbf52420cd192a9c6d5f676e369767e361c
sadiq <sabitsdq@gmail.com>
2023-07-11 18:38:51 +0100

    adding  bg-color to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import PostPackageIntro from "@/components/PostPackage/PostPackageIntro";
-import TripInfo from "@/components/PostPackage/TripInfo";
 import Link from "next/link";
 export default function Home() {
   return (
-    <div className="w-full">
-      <div className="w-11/12 md:w-1/2 grid grid-cols-3 md:grid-flow-row gap-3 md:gap-4 h-8 mx-auto  text-white">
+    <div className="w-full h-screen bg-[#000000]">
+      <div className="w-11/12 md:w-1/2 grid grid-cols-2 md:grid-flow-row gap-3 md:gap-4 h-8 mx-auto  text-white">
         <Link
           className="bg-gray-700 mt-4 opacity-50 rounded-lg px-4 flex self-center text-white py-2"
           href="/postPackage"
