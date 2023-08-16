b0d1376a3a7b3ab7208ab7ef9e9a5607eda716a1
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-08-16 20:04:49 +0100

    fixed underlying bug issue of colors not appearing for the btns

--- app/myPosting/page.tsx
+++ app/myPosting/page.tsx
@@ -1,29 +1,23 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { BiDotsVertical } from "react-icons/bi";
-import Number from "../components/DropMenu/TableDropdown";
-import { tableData } from "../components/constants/data";
-import { useMediaQuery } from "@react-hook/media-query";
+'use client';
+import React, { useEffect, useState } from 'react';
+import { BiDotsVertical } from 'react-icons/bi';
+import Number from '../components/DropMenu/TableDropdown';
+import { tableData } from '../components/constants/data';
+import { useMediaQuery } from '@react-hook/media-query';
 
 const page = () => {
   const [handleModal, setHandleModal] = useState(tableData);
   const btnColor: any = {
-    review: "#99E918",
-    posted: "#FFD700",
-    closed: "#FF5631",
-    booked: "#08E06C",
+    review: 'bg-[#99E918]',
+    posted: 'bg-[#FFD700]',
+    closed: 'bg-[#FF5631]',
+    booked: 'bg-[#08E06C]',
   };
   const toggledropDown = (id: string) => {
-    setHandleModal((prevValue) =>
-      prevValue.map((obj) =>
-        obj.id === id
-          ? { ...obj, modalOpen: !obj.modalOpen }
-          : { ...obj, modalOpen: false }
-      )
-    );
+    setHandleModal((prevValue) => prevValue.map((obj) => (obj.id === id ? { ...obj, modalOpen: !obj.modalOpen } : { ...obj, modalOpen: false })));
   };
 
-  const isSmallScreen = useMediaQuery("(max-width:480px)");
+  const isSmallScreen = useMediaQuery('(max-width:480px)');
 
   const isLastRow = tableData.length - 1;
   const isSecondtoLastRow = tableData.length - 2;
@@ -32,27 +26,15 @@ const page = () => {
     return (
       <tr key={obj.id}>
         <td className="px-6 py-2 whitespace-nowrap">{obj.location}</td>
-        <td className="px-6 py-2 whitespace-nowrap leading-5 text-[#61748D]">
-          {obj.type}
-        </td>
+        <td className="px-6 py-2 whitespace-nowrap leading-5 text-[#61748D]">{obj.type}</td>
         <td className="px-6 py-2 whitespace-nowrap leading-6">{obj.id}</td>
         <td className="px-6 py-2 whitespace-nowrap">
-          <button
-            className={`bg-[${
-              btnColor[obj.status.toLowerCase()]
-            }] opacity-80 rounded-3xl px-5 py-2`}
-          >
-            {obj.status}
-          </button>
+          <button className={`${btnColor[obj.status.toLowerCase()]} opacity-80 rounded-3xl px-5 py-2`}>{obj.status}</button>
         </td>
         <td className="flex justify-end px-6 py-2  whitespace-nowrap relative">
           <BiDotsVertical
             size={17}
-            className={`z-10 mr-5 cursor-pointer ${
-              isLastRow === index ? "lastRow" : ""
-            } ${isSecondtoLastRow === index ? "SecondtoLastRow" : ""} ${
-              isSmallScreen && isLastRow === index ? "mobileLastRow" : ""
-            }`}
+            className={`z-10 mr-5 cursor-pointer ${isLastRow === index ? 'lastRow' : ''} ${isSecondtoLastRow === index ? 'SecondtoLastRow' : ''} ${isSmallScreen && isLastRow === index ? 'mobileLastRow' : ''}`}
             onClick={() => toggledropDown(obj.id)}
           />
           {obj.modalOpen && <Number />}
@@ -70,15 +52,11 @@ const page = () => {
           <table className="table-auto w-full text-[#0F0400] ">
             <thead className="text-base leading-6 font-bold">
               <tr className="">
-                <th className="px-6 py-3 text-left  tracking-wider">
-                  Location
-                </th>
+                <th className="px-6 py-3 text-left  tracking-wider">Location</th>
                 <th className="px-6 py-3 text-left  tracking-wider">Type</th>
                 <th className="px-6 py-3 text-left tracking-wider">ID</th>
                 <th className="px-6 py-3 text-left tracking-wider">Status</th>
-                <th className="px-6 py-3 text-left tracking-wider flex justify-end">
-                  Menu
-                </th>
+                <th className="px-6 py-3 text-left tracking-wider flex justify-end">Menu</th>
               </tr>
             </thead>
             <tbody className="font-bold text-sm leading-5 my-10">{array}</tbody>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  options: {
-    safelist: ["#12d900", "#FFD700", "#12d900", "#FFD700"], // Add the classes you want to preserve
-  },
+
+  safelist: ['bg-[#993918]', 'bg-[#FFD700]', 'bg-[#FF5631]', 'bg-[#08E06C]'], // Add the classes you want to preserve
 
   theme: {
     extend: {},
